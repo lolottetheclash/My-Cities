fdc0788ae369b3f6592156062ce8f25c3a546cc2
lauren <laurenzolivier@gmail.com>
2021-02-15 21:22:25 +0100

    add mongodb and mongoose

---
+++ backend/src/database/db.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+// Loading environment variables
+dotenv.config();
+
+const mongoUri = process.env.MONGO_URI;
+
+// Connection to MongoDB
+const mongodbStart = async (): Promise<void> => {
+  try {
+    await mongoose.connect(`${mongoUri}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      autoIndex: true,
+    });
+    console.log('MongoDB is connected');
+  } catch (err) {
+    console.error(`An error occurred during MongoDB connection : ${err}`);
+  }
+};
+
+export default mongodbStart;
